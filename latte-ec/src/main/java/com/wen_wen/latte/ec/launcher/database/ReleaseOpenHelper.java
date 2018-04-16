f8aabac41e65fcd4cc9447fe2239733a00b7343f
159753ss <dayup9843@163.com>
2018-04-16 21:33:06 +0800

    greenDao init

---
+++ latte-ec/src/main/java/com/wen_wen/latte/ec/launcher/database/ReleaseOpenHelper.java
@@ -0,0 +1,15 @@
+package com.wen_wen.latte.ec.launcher.database;
+
+import android.content.Context;
+
+/**
+ * Created by WeLot on 2018/4/16.
+ */
+
+public class ReleaseOpenHelper  extends  DaoMaster.OpenHelper {
+    public ReleaseOpenHelper(Context context, String name) {
+        super(context, name);
+    }
+
+
+}
