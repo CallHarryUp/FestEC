456ed833323578a45ad7f5b742cde7349293ad48
159753ss <dayup9843@163.com>
2018-05-03 13:02:07 +0800

    图片选择器02

--- latte-ec/src/main/java/com/wen_wen/latte/ec/launcher/main/personal/order/ui/AutoPhotoLayout.java
+++ latte-ec/src/main/java/com/wen_wen/latte/ec/launcher/main/personal/order/ui/AutoPhotoLayout.java
@@ -2,6 +2,7 @@ package com.wen_wen.latte.ec.launcher.main.personal.order.ui;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.util.AttributeSet;
@@ -9,7 +10,9 @@ import android.view.View;
 
 import com.joanzapata.iconify.widget.IconTextView;
 import com.wen_wen.latte.app.delegate.LatteDelegate;
+import com.wen_wen.latte.ec.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -33,7 +36,10 @@ public class AutoPhotoLayout extends LinearLayoutCompat {
     private List<View> mLineViews;
     private AlertDialog mTargetDialog;
 
-
+    private static final String ICON_TEXT = "{fa-plis}";
+    private float mIconSize = 0;
+    private static final List<List<View>> ALL_VIEWS = new ArrayList<>();
+    private static final List<Integer> LINE_HEIGHTS = new ArrayList<>();
 
     public AutoPhotoLayout(Context context) {
         this(context, null);
@@ -45,6 +51,17 @@ public class AutoPhotoLayout extends LinearLayoutCompat {
 
     public AutoPhotoLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        //从xml文件中获取参数
+        final TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.camera_flow_layout);
+        mMaxNum = typedArray.getInt(R.styleable.camera_flow_layout_max_count, 1);
+        mMaxLineNum = typedArray.getInt(R.styleable.camera_flow_layout_line_count, 3);
+        mImageMargin = typedArray.getInt(R.styleable.camera_flow_layout_item_margin, 0);
+        mIconSize = typedArray.getInt(R.styleable.camera_flow_layout_icon_size, 20);
+        typedArray.recycle();
+
+
+
+
     }
 
 
